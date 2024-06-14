6d4da34c1e0fa8d815ce9ebd6ff40ba48fec3981
Davi Leal <davileal1401@outlook.com>
2024-06-14 11:22:22 -0300

    :rocket: complete ContaBanco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite seu Nome! ");
+        String nome = scanner.next();
+
+        System.out.println("Por favor, digite o número da sua Agencia! ");
+        String agencia = scanner.next();
+
+        System.out.println("Por favor, digite o número da sua Conta! ");
+        int conta = scanner.nextInt();
+
+        System.out.println("Por favor, digite seu Saldo! ");
+        double saldo = scanner.nextDouble();
+
+        String mensagem = "Olá ".concat(nome).concat(", obrigado por criar uma conta em nosso banco, sua agência é ")
+                .concat(agencia).concat(", conta ") + conta + " e seu saldo R$" + saldo
+                + " já está disponível para saque.";
+
+        System.out.println(mensagem);
+
+        scanner.close();
     }
 }
